ffbaf96b033d67e2ee5f3c1033dbb20460d01750
Aiden Gourley <18075859@brookes.ac.uk>
2020-02-09 16:04:02 +0000

    Moved Edit Profile UI into position

---
+++ app/src/main/java/com/example/cooknchill/auth/EditProfileActivity.java
@@ -0,0 +1,36 @@
+package com.example.cooknchill.auth;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cooknchill.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+
+public class EditProfileActivity extends AppCompatActivity {
+    Button btnChangePicture;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editprofile);
+        btnChangePicture = findViewById(R.id.changePicture);
+
+        btnChangePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().setPhotoUri(Uri.fromFile())
+                //user.updateProfile()
+            }
+        });
+    }
+}
